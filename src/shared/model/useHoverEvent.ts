f297f20257737e471c1c825f64351bc3af0f43ef
dinn54 <dinn54@imantisco.com>
2025-02-02 11:14:18 +0900

    feat : add problem CRUD form

---
+++ src/shared/model/useHoverEvent.ts
@@ -0,0 +1,23 @@
+'use client';
+import { useEffect, useRef, useCallback, RefObject } from 'react';
+
+export const useHoverEvent = (
+  ref: RefObject<HTMLDivElement | null>,
+  parentRef: RefObject<HTMLDivElement | null>,
+  callback: () => void,
+  unMountCallback: () => void
+) => {
+  if (!ref.current || !parentRef.current) return;
+  const element = ref.current;
+  const parentElement = parentRef.current;
+
+  element.addEventListener('mouseover', callback);
+  parentElement.addEventListener('mouseleave', unMountCallback);
+
+  return () => {
+    element.removeEventListener('mouseover', callback);
+    parentElement.removeEventListener('mouseleave', unMountCallback);
+  };
+};
+
+export default useHoverEvent;
