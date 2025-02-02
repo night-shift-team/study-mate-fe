4b72a89c717af01352c0061f8a624ece1e775183
dinn54 <dinn54@imantisco.com>
2025-02-02 13:31:24 +0900

    fix: fix dashobard grid contents margin x

--- src/page/solveProblem/ui/index.tsx
+++ src/page/solveProblem/ui/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const SolveProblem = () => {
   return (
     <div className="flex h-full w-full flex-col px-[6rem] py-[8rem]">
-      <div className="flex h-[50%] w-full px-[0.5rem]">
+      <div className="flex h-[50%] w-full">
         <div className="flex h-full w-[25%] items-center justify-center px-[0.5rem] text-[3vh]">
           Computer Science
         </div>
@@ -13,13 +13,13 @@ const SolveProblem = () => {
             <CategoryGridContents title="운영체제" count={152} />
             <CategoryGridContents title="데이터베이스" count={152} />
             <CategoryGridContents title="네트워크" count={152} />
-            <CategoryGridContents title="자료구조와 알고리즘" count={152} />
+            <CategoryGridContents title={`자료구조와\n알고리즘`} count={152} />
           </div>
         </div>
       </div>
-      <div className="flex h-[50%] w-full px-[0.5rem]">
+      <div className="flex h-[50%] w-full">
         <div className="flex h-full w-[75%] px-[3rem] py-[3rem]">
-          <div className="trans grid h-full w-full grid-flow-col justify-end gap-x-[3rem] overflow-x-scroll py-[0.3rem] scrollbar-hide">
+          <div className="grid h-full w-full grid-flow-col justify-end gap-x-[3rem] overflow-x-scroll py-[0.3rem] scrollbar-hide">
             <CategoryGridContents title="Javascript" count={152} />
             <CategoryGridContents title="React" count={152} />
             <CategoryGridContents title="Nextjs" count={152} />
@@ -53,13 +53,13 @@ const CategoryGridContents = ({
   };
   return (
     <Link
-      className="relative flex h-full w-fit text-[2.5vh]"
+      className="relative mx-[0.5rem] flex h-full w-fit text-[2.5vh]"
       href={`/solveproblem/${title}`}
     >
       <div
         className={`flex aspect-square h-full flex-col rounded-[2rem] p-[5%] ${bgColor()} z-[1] items-center justify-center`}
       >
-        <span>{title}</span>
+        <span className="flex whitespace-pre-wrap text-center">{title}</span>
         <span>{`(${count})`}</span>
       </div>
       <div className="absolute left-[3%] top-[3%] flex aspect-square h-full rounded-[2rem] bg-[#D9D9D9]" />
