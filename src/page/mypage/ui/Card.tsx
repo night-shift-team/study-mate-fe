c7127069e8f4589983e7cf4db04b69edd164a5a6
yeonsu <rhdustn@imantisco.com>
2025-02-03 11:57:14 +0900

    feat: update mypage css

--- src/page/mypage/ui/Card.tsx
+++ src/page/mypage/ui/Card.tsx
@@ -8,14 +8,14 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ count, label, img }) => {
   return (
-    <div className="flex h-[70px] w-[100%] flex-col items-center justify-center rounded-xl bg-white p-3 shadow-md md:h-[100px] md:w-[25%] md:p-4">
+    <div className="flex h-[8vh] w-[100%] flex-col items-center justify-center rounded-xl bg-white p-3 shadow-md md:h-[100px] md:w-[25%] md:p-4">
       <div className="flex items-center gap-2">
-        <span className="flex h-6 w-6 items-center justify-center text-xl text-[#FEA1A1] md:h-8 md:w-8 md:text-2xl">
+        <span className="flexitems-center justify-center text-[2vh] text-[#FEA1A1]">
           {img}
         </span>
-        <span className="text-sm text-gray-600 md:text-base">{label}</span>
+        <span className="text-[2vh] text-gray-600 md:text-base">{label}</span>
       </div>
-      <span className="text-2xl font-bold md:text-3xl">{count}</span>
+      <span className="text-[2.5vh] font-bold md:text-3xl">{count}</span>
     </div>
   );
 };
--- src/page/mypage/ui/index.tsx
+++ src/page/mypage/ui/index.tsx
@@ -19,11 +19,11 @@ const Mypage = () => {
   const [tap, setTap] = useState('1');
 
   return (
-    <div className="flex w-full flex-col items-center md:px-[5%] md:pt-10">
-      <div className="relative h-[25vh] w-full flex-col items-center bg-[#FEA1A1] px-4 py-7 md:h-[20vh] md:rounded-t-3xl">
+    <div className="flex h-screen w-screen flex-col items-center md:px-[5%] md:pt-10">
+      <div className="relative h-[20vh] w-full flex-col items-center justify-center bg-[#FEA1A1] px-4 py-4 md:rounded-t-3xl">
         <Profile />
 
-        <div className="mx-auto mt-[2vh] grid w-[100%] grid-cols-2 justify-items-center gap-2 md:absolute md:left-[20%] md:top-[60%] md:mt-0 md:flex md:flex-1 md:gap-4">
+        <div className="mt-2 grid w-[100%] grid-cols-2 justify-items-center gap-2 md:absolute md:left-[20%] md:top-[60%] md:mt-0 md:flex md:flex-1 md:gap-4">
           {cardData.map((item, index) => (
             <Card
               key={index}
@@ -35,14 +35,14 @@ const Mypage = () => {
         </div>
       </div>
 
-      <div className="flex h-full w-full flex-col gap-3 bg-[#F9FBE7] px-4 py-4 pt-[120px] md:pt-[10%]">
-        <div className="max-w-full md:flex md:gap-3">
-          <button className="text-ㄴ mb-2 w-[10%] text-nowrap text-[2vh] font-bold text-[#ECCDB4]">
+      <div className="flex h-[80vh] w-full flex-col gap-2 bg-[#F9FBE7] px-4 py-2 pt-[80px] md:pt-[10%]">
+        <div className="mt-[6vh] h-1/2 max-w-full sm:mt-0 md:flex md:gap-3">
+          <button className="mb-2 w-[10%] text-nowrap text-[2vh] font-bold text-[#ECCDB4]">
             활동기록
           </button>
           <GrassChart />
         </div>
-        <div className="max-w-full md:flex md:gap-3">
+        <div className="h-1/2 max-w-full md:flex md:gap-3">
           <div className="flex w-[10%] gap-4 md:flex-col">
             <button
               onClick={() => setTap('1')}
