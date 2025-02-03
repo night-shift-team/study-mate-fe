ffb5dfc85fd6733b046f979874ed02999db9bdea
dinn54 <dinn54@imantisco.com>
2025-02-03 15:43:01 +0900

    feat : add google auth scope email

--- src/entities/index.tsx
+++ src/entities/index.tsx
@@ -8,7 +8,7 @@ export const LoginButton = [
     id: 2,
     img: google,
     title: 'google',
-    link: 'https://accounts.google.com/o/oauth2/v2/auth?client_id=877767008504-iq3dn2jdum3dhilejajhhotek0rdelts.apps.googleusercontent.com&redirect_uri=http://localhost:8080/login/oauth2/code/google&response_type=code&scope=profile',
+    link: 'https://accounts.google.com/o/oauth2/v2/auth?client_id=877767008504-iq3dn2jdum3dhilejajhhotek0rdelts.apps.googleusercontent.com&redirect_uri=http://localhost:8080/login/oauth2/code/google&response_type=code&scope=email profile',
   },
   { id: 3, img: github, title: 'github', link: '' },
 ];
