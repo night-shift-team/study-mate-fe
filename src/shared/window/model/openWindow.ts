79865b7a464e2c89e20699f43a1a75055c181a9f
dinn54 <dinn54@imantisco.com>
2025-02-04 11:57:53 +0900

    fix: fix google auth new window location

--- src/shared/window/model/openWindow.ts
+++ src/shared/window/model/openWindow.ts
@@ -1,13 +1,33 @@
+interface ExtScreen extends Screen {
+  availTop: number;
+  availLeft: number;
+}
+
 export const openNewWindowWithoutDuplicate = (
   windowReference: Window | null,
   link: string
 ) => {
+  const windowWidth = 400;
+  const windowHeight = 650;
+
+  const top =
+    (window.screen.availHeight - windowHeight) / 2 +
+    (window.screen as ExtScreen).availTop;
+  const left =
+    (window.screen.availWidth - windowWidth) / 2 +
+    (window.screen as ExtScreen).availLeft;
+
+  console.log(window.screen);
   if (
     !windowReference ||
     windowReference.closed === undefined ||
     windowReference.closed
   ) {
-    windowReference = window.open(link, link, 'width=400,height=650');
+    windowReference = window.open(
+      link,
+      link,
+      `width=${windowWidth},height=${windowHeight},top=${top},left=${left}`
+    );
   } else {
     windowReference.location.href = link;
     windowReference.focus();
