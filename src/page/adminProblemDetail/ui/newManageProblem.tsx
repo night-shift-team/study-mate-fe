0def274f00893b9ab54098ce28b989afb1ed5264
dinn54 <dinn543210@gmail.com>
2025-02-25 21:20:40 +0900

    feat: add problem management page

--- src/page/adminProblemDetail/ui/newManageProblem.tsx
+++ src/page/adminProblemDetail/ui/newManageProblem.tsx
@@ -30,50 +30,80 @@ const NewManageProlem = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const LAST_PAGE = 10;
   return (
-    <div className="flex h-full w-full flex-col border p-4">
+    <div className="relative flex h-full w-full flex-col overflow-y-auto p-4 scrollbar-hide">
       <span className="flex w-full justify-center text-[1.4rem]">
         문제 관리
       </span>
-      <div className="mt-4 flex w-full justify-between">
-        <ProblemFilterComponent />
-        <ProblemSearchComponent />
-      </div>
-      {ProblemList.map((problem, index) => {
-        return (
-          <div
-            key={index}
-            className="mt-2 flex h-[2.5rem] w-full items-center justify-between rounded-xl border bg-gray-300 p-1.5 px-3"
-            onClick={(e) => {
-              setSelectedProblem(problem.id);
-            }}
-          >
-            <span className="hover:cursor-pointer hover:underline">
-              {problem.id} {problem.title} {problem.date}
-            </span>
-            <div className="flex h-full w-20 gap-1 text-xs">
-              <div
-                className="flex h-full w-1/2 items-center justify-center rounded-md border border-black hover:cursor-pointer"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  console.log('child');
-                }}
-              >
-                수정
-              </div>
-              <div
-                className="flex h-full w-1/2 items-center justify-center rounded-md border border-black hover:cursor-pointer"
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
-              >
-                삭제
+      <span className="absolute left-4 top-3 flex w-20 items-center justify-center whitespace-nowrap rounded-xl border bg-gray-200 p-2 text-sm">
+        문제 생성
+      </span>
+      <div className="mt-8 flex w-full flex-col">
+        <div className="flex w-full justify-between">
+          <ProblemFilterComponent />
+          <ProblemSearchComponent />
+        </div>
+        {ProblemList.map((problem, index) => {
+          return (
+            <div
+              key={index}
+              className="mt-2 flex h-[2.5rem] w-full items-center justify-between rounded-xl border bg-gray-300 p-1.5 px-3"
+              onClick={(e) => {
+                setSelectedProblem(problem.id);
+              }}
+            >
+              <span className="hover:cursor-pointer hover:underline">
+                {problem.id} {problem.title} {problem.date}
+              </span>
+              <div className="flex h-full w-20 gap-1 text-xs">
+                <div
+                  className="flex h-full w-1/2 items-center justify-center rounded-md border border-black hover:cursor-pointer"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    console.log('child');
+                  }}
+                >
+                  수정
+                </div>
+                <div
+                  className="flex h-full w-1/2 items-center justify-center rounded-md border border-black hover:cursor-pointer"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                  삭제
+                </div>
               </div>
             </div>
+          );
+        })}
+        <div className="mt-4 flex w-full justify-center">
+          <ProblemPagination
+            data={{ currentPage, LAST_PAGE, setCurrentPage }}
+          />
+        </div>
+      </div>
+      <div className="flex w-full flex-col">
+        <span className="mt-8 flex w-full justify-center text-[1.4rem]">
+          {' '}
+          Preview
+        </span>
+        <div className="relative mt-4 flex h-[33rem] w-full flex-col rounded-2xl bg-gray-200 p-6">
+          <span className="text-lg font-bold">Title</span>
+          <span className="mt-2 text-xs">
+            {
+              '다음 중 객체지향 프로그래밍의 4가지 주요 특징에 포함되지 않는 것은?'
+            }
+          </span>
+          <span className="mt-4 text-lg font-bold">Contents</span>
+          <div className="h-[60%] w-full border border-black">
+            {'//TODO: MarkDown insert'}
           </div>
-        );
-      })}
-      <div className="mt-4 flex w-full justify-center">
-        <ProblemPagination data={{ currentPage, LAST_PAGE, setCurrentPage }} />
+          <div className="absolute bottom-4 left-0 flex w-full justify-center">
+            <span className="flex items-center justify-center rounded-xl px-3 pb-1 pt-2 text-sm hover:bg-gray-100 hover:underline">
+              자세히 보기
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
