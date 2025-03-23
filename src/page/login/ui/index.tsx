d098ac0f8f57f292b9550a6c2d1a274827f70f2b
dinn54 <dinn543210@gmail.com>
2025-03-23 15:46:30 +0900

    style: update login page mobile responsive screen

--- src/page/login/ui/index.tsx
+++ src/page/login/ui/index.tsx
@@ -130,27 +130,29 @@ const Login = () => {
   }, []);
 
   return (
-    <div className="relative flex h-full w-full items-center justify-center overflow-x-hidden pb-[4rem]">
+    <div className="relative z-[1000] flex h-full w-full items-center justify-center overflow-hidden md:pb-[4rem]">
       <Toaster />
-      <div className="inner-border-left flex w-full max-w-[900px] flex-col justify-around gap-6 rounded-[1rem] border bg-white/60 p-8 shadow-xl md:flex-row">
+      <div className="md:inner-border-left flex w-full max-w-[900px] flex-col justify-around gap-4 rounded-[1rem] border bg-white/60 px-8 py-4 shadow-xl md:flex-row md:gap-6 md:p-8">
         <div className="flex flex-col justify-center md:items-center">
-          <Image src={Logo} alt="" width={240} />
+          <Image src={Logo} alt="" className="w-[120px] md:w-[240px]" />
           <div className="flex w-full flex-col">
-            <span className="text-2xl font-bold text-pointcolor-apricot">
+            <span className="text-lg font-bold text-pointcolor-apricot md:text-2xl">
               Study Mate
             </span>
-            <span className="mt-1 max-w-[240px]">
+            <span className="mt-1 break-words text-sm md:max-w-[240px] md:text-base">
               Online platform for solving computer science problems and
               enhancing learning.
             </span>
           </div>
         </div>
-        <div className="flex flex-col gap-10 sm:gap-6 md:w-[50%]">
-          <span className="font-doodle text-3xl font-bold">Welcome</span>
+        <div className="flex flex-col gap-4 sm:gap-6 md:w-[50%] md:gap-10">
+          <span className="font-doodle text-xl font-bold md:text-3xl">
+            Welcome
+          </span>
           <form
             id={'loginForm'}
             onSubmit={handleSubmit}
-            className="flex flex-col gap-4"
+            className="flex flex-col gap-2 md:gap-4"
           >
             <div className="flex w-[100%] flex-col">
               <span className="text-[0.8rem]">Email</span>
@@ -196,7 +198,7 @@ const Login = () => {
               id="loginButton"
               type="submit"
               disabled={loginLoading}
-              className={`flex h-[2.5rem] w-full items-center justify-center rounded-lg bg-gray-600 py-2 text-white transition-colors ${loginLoading ? 'hover:bg-gray-400' : 'hover:bg-[#F0EDD4] hover:text-black'}`}
+              className={`flex h-[2.5rem] w-full items-center justify-center rounded-lg bg-gray-600 py-1 text-white transition-colors md:py-2 ${loginLoading ? 'hover:bg-gray-400' : 'hover:bg-[#F0EDD4] hover:text-black'}`}
             >
               {loginLoading ? <Spinner /> : '로그인'}
             </button>{' '}
@@ -216,7 +218,7 @@ const Login = () => {
                 <div className="w-full border-t border-gray-300"></div>
               </div>
               <div className="relative flex justify-center text-sm">
-                <span className="bg-[#fcfdf6] px-2 text-gray-500">
+                <span className="bg-[#fcfdf6] px-2 text-xs text-gray-500 md:text-sm">
                   간편 로그인
                 </span>
               </div>
@@ -229,7 +231,7 @@ const Login = () => {
                   onClick={() => {
                     openNewWindowWithoutDuplicate(windowReference, item.link);
                   }}
-                  className="flex h-[5rem] w-1/3 cursor-pointer flex-col justify-center"
+                  className="flex h-[4rem] w-1/3 cursor-pointer flex-col justify-center md:h-[5rem]"
                 >
                   <div className="flex justify-center rounded-md border-2 py-1">
                     <Image
