9dfe99b00ddc0c20267e39c5aacb4437a6514da7
dinn54 <dinn543210@gmail.com>
2025-04-15 16:37:25 +0900

    feat : add notice page pagination

--- src/app/announcement/page.tsx
+++ src/app/announcement/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { ProblemPagination } from '@/feature/adminProblem/ui/manageProblemComponents';
 import {
   getAllNoticeListApi,
   getAllNoticeListRes,
@@ -21,6 +22,7 @@ enum AnnouncementType {
 
 const AnnouncementPage = () => {
   const [currentTab, setCurrentTab] = useState<AnnouncementType>(0);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getNoticeList = (currentTab: AnnouncementType) => {
     if (currentTab === AnnouncementType.Anouncement) {
@@ -33,39 +35,35 @@ const AnnouncementPage = () => {
     getNoticeList(currentTab)
   );
   const [page, setPage] = useState(0);
+  const [maxPage, setMaxPage] = useState(1);
   const MAX_PAGE = 10;
 
   const getNoticeListByPage = async (page: number) => {
+    setIsLoading(true);
+
     try {
       const res = await getAllNoticeListApi(page, MAX_PAGE);
       if (res.ok) {
         const noticeList = (res.payload as getAllNoticeListRes).content;
+        setMaxPage((res.payload as getAllNoticeListRes).totalPages);
         setAnnouncementList(noticeList);
       }
     } catch (e) {
       console.log(e);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    if (announcementList.length === 0) {
-      const storedAnnouncementList = sessionStorage.getItem('validNoticeList');
-      if (storedAnnouncementList) {
-        try {
-          setAnnouncementList(JSON.parse(storedAnnouncementList) as Notice[]);
-        } catch (e) {
-          console.log(e);
-          setAnnouncementList([] as Notice[]);
-        }
-      } else {
-        getNoticeListByPage(page);
-      }
+    if (currentTab === AnnouncementType.Anouncement) {
+      getNoticeListByPage(page);
+    } else {
+      setAnnouncementList([]);
+      setPage(0);
+      setMaxPage(1);
     }
-  }, []);
-
-  useEffect(() => {
-    //TODO: tab에 따라 공지사항 리스트를 변경
-  }, [currentTab]);
+  }, [currentTab, page]);
 
   return (
     <div className="flex h-full w-full flex-col p-20">
@@ -75,28 +73,43 @@ const AnnouncementPage = () => {
           {/* Announcement , Event Tab */}
           <div className="flex w-[8rem] flex-col gap-3 p-2">
             <button
+              disabled={isLoading}
               onClick={() => setCurrentTab(AnnouncementType.Anouncement)}
               className={`py-1 text-center text-lg font-bold ${currentTab === AnnouncementType.Anouncement ? 'bg-pointcolor-beigebrown/50 shadow-sm' : ''} rounded-xl`}
             >
               공지사항
             </button>
             <button
+              disabled={isLoading}
               onClick={() => setCurrentTab(AnnouncementType.Event)}
-              className={`py-1 text-center text-lg font-bold ${currentTab === AnnouncementType.Event ? 'bg-pointcolor-beigebrown/50 shadow-sm' : ''} rounded-xl`}
+              className={`py-1 text-center text-lg ${isLoading ? 'text-gray-500' : 'font-bold'} ${currentTab === AnnouncementType.Event ? 'bg-pointcolor-beigebrown/50 shadow-sm' : ''} rounded-xl`}
             >
               이벤트
             </button>
           </div>
           <div className="flex h-full w-full flex-col gap-4 px-3 py-1">
-            {announcementList.map((announcement) => {
-              return (
-                <NoticeDataRow
-                  key={announcement.noticeId}
-                  noticeDetail={announcement}
-                />
-              );
-            })}
+            {announcementList.length ? (
+              announcementList.map((announcement) => {
+                return (
+                  <NoticeDataRow
+                    key={announcement.noticeId}
+                    noticeDetail={announcement}
+                  />
+                );
+              })
+            ) : (
+              <div className="flex h-full w-full items-center justify-center">
+                <span className="text-xl">No data</span>
+              </div>
+            )}
           </div>
+        </div>
+        <div className="flex h-[4rem] w-full justify-center pl-[8rem]">
+          <ProblemPagination
+            page={page}
+            setPage={setPage}
+            paginationSize={maxPage}
+          />
         </div>
       </div>
     </div>
