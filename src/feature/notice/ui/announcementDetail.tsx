18dd703d62549cd0d0ae57181718a11424e10f37
dinn54 <dinn543210@gmail.com>
2025-04-24 20:59:34 +0900

    style: update notice detail ui

--- src/feature/notice/ui/announcementDetail.tsx
+++ src/feature/notice/ui/announcementDetail.tsx
@@ -9,6 +9,7 @@ import Image from 'next/image';
 import listMenu from '@/assets/icons/list-text.png';
 import { useRouter } from 'next/navigation';
 import { RouteTo } from '@/shared/routes/model/getRoutePath';
+import NoticeFoxBg from '@/assets/noticeFoxBg.svg';
 
 const AnnouncemnetByIdDetail = ({
   id,
@@ -38,18 +39,30 @@ const AnnouncemnetByIdDetail = ({
   }
 
   return (
-    <div className="relative flex h-full w-[50rem] flex-col items-center gap-8 overflow-y-auto p-7 scrollbar-hide md:p-20">
-      <div className="absolute left-5 flex h-[3rem] w-full md:left-0">
+    <div className="relative flex h-full w-[50rem] flex-col items-center gap-8 overflow-y-auto p-7 scrollbar-hide md:px-20 md:py-20">
+      <div className="absolute left-1 top-11 flex h-[4rem] w-full md:left-0 md:top-[5.5em]">
         <button
-          className="flex h-full w-[3rem] rounded-2xl p-3 hover:bg-pointcolor-beigebrown/20 md:p-2.5"
+          className="relative flex h-full w-full gap-1 rounded-2xl p-2 md:gap-2 md:p-2.5"
           onClick={() => router.push(RouteTo.Announcement)}
         >
-          <Image src={listMenu} alt="list-text" />
+          <Image
+            src={listMenu}
+            alt="list-text"
+            className="h-fit w-[3.5rem] rounded-full p-2 px-4 hover:bg-pointcolor-beigebrown/20 md:w-[3.8rem]"
+            objectFit="cover"
+          />
+          <span className="font-doodle text-3xl md:text-4xl">Notice</span>
         </button>
       </div>
+      <div className="absolute right-10 top-8 flex h-[5rem] w-fit md:right-[5rem] md:top-4 md:h-[10.2rem]">
+        <Image
+          src={NoticeFoxBg}
+          alt="noticeFoxBg"
+          className="h-full w-full drop-shadow-lg"
+        />
+      </div>
 
-      <span className="font-doodle text-3xl md:text-4xl">Notice</span>
-      <div className="flex min-h-[30rem] w-full flex-col items-center gap-3 rounded-3xl bg-white p-6 text-sm shadow-md md:gap-4 md:text-base">
+      <div className="z-[1] mt-[5rem] flex min-h-[30rem] w-full flex-col items-center gap-3 rounded-3xl bg-white p-6 text-sm shadow-md md:mt-[6rem] md:gap-4 md:text-base">
         <span className="flex w-full justify-center font-gmarketsans">
           {noticeDetail &&
             convertNoticeCategoryToString(noticeDetail?.noticeCategory)}
