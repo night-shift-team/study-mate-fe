46b2ca27e4ba64712b9c7ca0bd3445cee9f20d60
dinn54 <dinn543210@gmail.com>
2025-05-13 20:58:27 +0900

    fix: fix toaster document exist

--- src/shared/toast/toast.tsx
+++ src/shared/toast/toast.tsx
@@ -90,6 +90,8 @@ const useToast = (
     status?: ToastStatus;
     description?: string;
   }) => {
+    if (!document) return null;
+
     return createPortal(
       <div
         className={`fixed left-1/2 top-[3.5rem] z-50 flex h-[2.7rem] w-auto min-w-[5rem] max-w-[95vw] items-center gap-1 rounded-xl py-[1.35rem] text-[0.9rem] md:top-1 ${getToastBackgroundColor(status ?? toastType)} rounded-sm border px-4 shadow-light md:top-16 md:h-[2.9rem] md:justify-center ${animationClass}`}
