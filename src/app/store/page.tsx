9b709de34d46acc9b67589118e12680e4697374e
dinn54 <dinn543210@gmail.com>
2025-05-30 11:52:32 +0900

    feat : add store cart icon

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -2,6 +2,7 @@ import Panel from '@public/assets/backgroundImages/store/storePanel2.svg';
 import Image from 'next/image';
 import ItemCard from './storeItemCard';
 import ShieldIcon from '@public/assets/icons/store/shieldIcon3.png';
+import CartIcon from '@public/assets/icons/store/cartIcon.png';
 import PurchaseHistory from '@public/assets/icons/store/purchaseHistory.png';
 import Link from 'next/link';
 import { RouteTo } from '@/shared/routes/model/getRoutePath';
@@ -13,17 +14,33 @@ const StorePage = () => {
         <div className="mt-4 flex h-[8rem] w-full justify-center">
           <Panel className="h-full w-full scale-[1.45] object-contain" />
         </div>
+        <button
+          aria-label="cart"
+          className="absolute right-2 top-0 flex h-[3rem] w-[2.8rem] flex-col md:right-5 md:top-3 md:h-[3.8rem] md:w-[3.6rem]"
+        >
+          <div className="relative flex h-full w-full">
+            <div className="absolute flex aspect-1 w-full">
+              <Image src={CartIcon} fill alt="cart_icon" />
+            </div>
+            <div className="absolute bottom-2 right-1 flex aspect-1 h-3.5 items-center justify-center rounded-full bg-[#ef4444] text-[0.6rem] font-semibold text-white md:bottom-[0.65rem] md:right-1.5 md:h-[1.1rem] md:text-[0.65rem]">
+              1
+            </div>
+            <span className="absolute -bottom-[0.1rem] flex w-full justify-center text-[0.5rem] tracking-tighter md:text-[0.65rem] md:tracking-tight">
+              장바구니
+            </span>
+          </div>
+        </button>
         <Link
           aria-label="purchase_history"
           href={RouteTo.StorePurchaseHistory}
-          className="absolute right-5 top-4 flex h-[5rem] w-[3.6rem] flex-col"
+          className="absolute right-2 top-[3.2rem] flex h-[3rem] w-[2.8rem] flex-col md:right-5 md:top-20 md:h-[3.8rem] md:w-[3.6rem]"
         >
           <div className="relative flex h-full w-full">
             <div className="absolute flex aspect-1 w-full">
               <Image src={PurchaseHistory} fill alt="purchase_history" />
             </div>
-            <span className="tracking-tights absolute bottom-4 flex w-full justify-center text-[0.65rem]">
-              구매 내역
+            <span className="absolute -bottom-[0.1rem] flex w-full justify-center text-[0.5rem] tracking-tighter md:text-[0.65rem] md:tracking-tight">
+              구매내역
             </span>
           </div>
         </Link>
