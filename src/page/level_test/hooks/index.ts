887df78572252da2c71a01536abc62567d5107e0
yeonsu <127164623+rhdustn@users.noreply.github.com>
2025-06-20 01:28:52 +0900

    feat : update useChacingLevelTest logic

--- src/page/level_test/hooks/index.ts
+++ src/page/level_test/hooks/index.ts
@@ -2,25 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import { getLevelTestQuestionsApi } from '../api';
 import { ProblemInfoLevelTest } from '@/shared/constants/problemInfo';
 
-export const useChachingLevelTest = () => {
+export const useChacingLevelTest = () => {
   return useQuery<ProblemInfoLevelTest[], Error>({
     queryKey: ['levelTestQuestions'],
     queryFn: async () => {
       const res = await getLevelTestQuestionsApi();
       if (!res.ok) {
-        throw new Error(
-          (res.payload as { message?: string })?.message ||
-            '문제를 불러오지 못했습니다. 서버 오류.'
-        );
+        throw new Error(error.message);
       }
-
-      if (!Array.isArray(res.payload)) {
-        throw new Error(
-          `문제 목록이 배열이 아닙니다: ${JSON.stringify(res.payload)}`
-        );
-      }
-
-      return res.payload;
+      return res.payload as ProblemInfoLevelTest[];
     },
     staleTime: 1000 * 60 * 5,
     retry: 1,
