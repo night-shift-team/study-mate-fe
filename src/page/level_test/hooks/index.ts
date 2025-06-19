f712a19b13120e7b1e9b18480e5d6f3a306168da
yeonsu <127164623+rhdustn@users.noreply.github.com>
2025-06-20 01:57:11 +0900

    feat : update leveltest chaching logic

--- src/page/level_test/hooks/index.ts
+++ src/page/level_test/hooks/index.ts
@@ -1,15 +1,27 @@
 import { useQuery } from '@tanstack/react-query';
 import { getLevelTestQuestionsApi } from '../api';
 import { ProblemInfoLevelTest } from '@/shared/constants/problemInfo';
+import { ServerErrorResponse } from '@/shared/api/model/config';
 
-export const useChacingLevelTest = () => {
+export type ApiResponse<T> = {
+  ok: boolean;
+  payload: ServerErrorResponse | T;
+};
+
+export const useChachingLevelTest = () => {
   return useQuery<ProblemInfoLevelTest[], Error>({
     queryKey: ['levelTestQuestions'],
     queryFn: async () => {
-      const res = await getLevelTestQuestionsApi();
+      const res: ApiResponse<ProblemInfoLevelTest[]> =
+        await getLevelTestQuestionsApi();
+
       if (!res.ok) {
-        throw new Error(error.message);
+        const errorPayload = res.payload as ServerErrorResponse;
+        throw new Error(
+          errorPayload.message || '레벨 테스트 문제를 불러오지 못했습니다.'
+        );
       }
+
       return res.payload as ProblemInfoLevelTest[];
     },
     staleTime: 1000 * 60 * 5,
