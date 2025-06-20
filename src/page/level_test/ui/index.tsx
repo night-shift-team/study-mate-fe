8604056118ad335f167f5ad96edf400fd691e3dc
dinn54 <dinn543210@gmail.com>
2025-06-20 18:50:19 +0900

    style: fix leveltest form layout

--- src/page/level_test/ui/index.tsx
+++ src/page/level_test/ui/index.tsx
@@ -165,7 +165,7 @@ const LevelTest = () => {
           }}
         />
       ) : (
-        <div className="flex h-full w-full max-w-[1200px] items-center justify-center rounded-xl px-[2%] py-[2%] md:max-h-[80%] md:min-h-[50vh] md:border md:border-pointcolor-sand md:shadow-lg">
+        <div className="flex h-full w-full max-w-[1200px] flex-col rounded-xl px-[2%] pb-[2%] pt-[1%] md:max-h-full md:min-h-[50vh]">
           <div className="flex h-full w-full flex-col gap-4 pb-4 md:pb-0">
             <div className="flex w-full items-center justify-between">
               <span className="pl-2">문제 {currentQuestionNo + 1}</span>{' '}
