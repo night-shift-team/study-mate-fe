5a32b0139cc1506a8fb27d0441b02a480ec650cd
yeonsu <127164623+rhdustn@users.noreply.github.com>
2025-06-20 14:18:27 +0900

    feat : update useChachingLevelTest hooks

--- src/page/level_test/hooks/index.ts
+++ src/page/level_test/hooks/index.ts
@@ -12,14 +12,20 @@ export const useChachingLevelTest = () => {
   return useQuery<ProblemInfoLevelTest[], Error>({
     queryKey: ['levelTestQuestions'],
     queryFn: async () => {
-      const res = await getLevelTestQuestionsApi();
-      if (res.ok) {
-        return res.payload as ProblemInfoLevelTest[];
-      }
+      try {
+        const res: ApiResponse<ProblemInfoLevelTest[]> =
+          await getLevelTestQuestionsApi();
 
-      throw res.payload;
+        if (res.ok) {
+          return res.payload as ProblemInfoLevelTest[];
+        }
+
+        throw res.payload;
+      } catch (e) {
+        console.log('레벨 테스트 문제 로딩 실패:', e);
+      }
     },
-    staleTime: 1000 * 60 * 5,
+    staleTime: 1000 * 60 * 5, // 5분
     retry: 1,
   });
 };
