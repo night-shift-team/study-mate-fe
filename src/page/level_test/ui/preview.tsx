8c8dc9b02be8e1987e40a118bf1d4b494be3c525
dinn54 <dinn543210@gmail.com>
2025-06-30 21:30:10 +0900

    style: decrease leveltest text gap

--- src/page/level_test/ui/preview.tsx
+++ src/page/level_test/ui/preview.tsx
@@ -9,7 +9,7 @@ export const Preview = ({ onStart }: PreviewProps) => {
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <h3>안녕하세요 {user?.nickname} 님!</h3>
-      <h2 className="mt-4 text-2xl font-bold">스터디메이트에 오신 것을</h2>
+      <h2 className="mt-2 text-2xl font-bold">스터디메이트에 오신 것을</h2>
       <h2 className="mb-4 text-2xl font-bold">환영합니다!</h2>
       <button
         onClick={onStart}
