20e66e38d71d6b4bf44d6a86251ad6752e965223
dddga <dga00agd@gmail.com>
2025-06-30 23:17:49 +0900

    chore: test commit

--- src/app/announcement/[id]/page.tsx
+++ src/app/announcement/[id]/page.tsx
@@ -10,7 +10,6 @@ const ClientAnnouncementDetail = () => {
   let data;
   useEffect(() => {
     const noticeData = sessionStorage.getItem('currentNoticeData');
-
     try {
       data = noticeData ? (JSON.parse(noticeData) as Notice) : undefined;
     } catch {
