1f30b4a02232f53909f1a471394917c2addb5158
dinn54 <dinn543210@gmail.com>
2025-06-30 21:25:11 +0900

    fix: fix initial leveltest notice letter

--- src/page/level_test/ui/preview.tsx
+++ src/page/level_test/ui/preview.tsx
@@ -9,9 +9,8 @@ export const Preview = ({ onStart }: PreviewProps) => {
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <h3>안녕하세요 {user?.nickname} 님!</h3>
-      <h2 className="mb-4 text-2xl font-bold">
-        레벨 테스트에 오신 것을 환영합니다
-      </h2>
+      <h2 className="mt-4 text-2xl font-bold">스터디메이트에 오신 것을</h2>
+      <h2 className="mb-4 text-2xl font-bold">환영합니다!</h2>
       <button
         onClick={onStart}
         className="rounded-lg bg-blue-300 px-4 py-2 font-semibold text-white transition duration-300 hover:bg-blue-400"
