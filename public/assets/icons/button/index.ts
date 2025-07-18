77afa228be1d3c6d79f395ff244d0eab9c800fbd
yeonsu <127164623+rhdustn@users.noreply.github.com>
2025-07-18 21:41:00 +0900

    feat : update design assests

--- public/assets/icons/button/index.ts
+++ public/assets/icons/button/index.ts
@@ -2,3 +2,4 @@ export { default as CorrectIcon } from './correct.png';
 export { default as WrongIcon } from './wrong.png';
 export { default as OnIcon } from './on.png';
 export { default as OffIcon } from './off.png';
+export { default as OpenIcon } from './open.png';
