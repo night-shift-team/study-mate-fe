4be1fb7f2717b05640ff60f29f4a99e50abd45d7
dinn54 <dinn543210@gmail.com>
2025-07-30 12:54:59 +0900

    fix: add metadata home canonical link

--- src/pageMetadata.ts
+++ src/pageMetadata.ts
@@ -21,28 +21,10 @@ export const pageMetadata = {
         },
       ],
     },
+    alternates: {
+      canonical: baseUrl,
+    },
   } as Metadata,
-  // solve: {
-  //   title: '문제 풀기 - 카테고리별 실전 연습',
-  //   description:
-  //     'Network, OS, DB, 알고리즘 문제를 직접 풀어보며 실력을 쌓아보세요.',
-  //   openGraph: {
-  //     title: '문제 풀기',
-  //     description:
-  //       '카테고리별 실전 문제를 풀며 면접 대비와 기본기 점검을 동시에!',
-  //     url: `${baseUrl}/solve`,
-  //     type: 'website',
-  //     images: [
-  //       {
-  //         url: `${baseUrl}/assets/backgroundImages/main/noticeBg.svg`,
-  //         alt: '문제 풀기 페이지 대표 이미지',
-  //       },
-  //     ],
-  //   },
-  //   alternates: {
-  //     canonical: `${baseUrl}/solve`,
-  //   },
-  // } as Metadata,
   suggestion: {
     applicationName: 'StudyMate',
     title: '건의 사항',
@@ -89,25 +71,6 @@ export const pageMetadata = {
       canonical: `${baseUrl}/rank`,
     },
   },
-  // store: {
-  //   title: '상점 - 학습을 더 즐겁게!',
-  //   description: '아이템을 사용하여 학습을 더 진행하고 순위를 올려 보세요!',
-  //   openGraph: {
-  //     title: 'StudyMate 상점',
-  //     description: '아이템을 사용하여 학습을 더 진행하고 순위를 올려 보세요!',
-  //     url: `${baseUrl}/store`,
-  //     type: 'website',
-  //     images: [
-  //       {
-  //         url: `${baseUrl}/assets/backgroundImages/store/storePanel2.svg`,
-  //         alt: '상점 페이지 이미지',
-  //       },
-  //     ],
-  //   },
-  //   alternates: {
-  //     canonical: `${baseUrl}/store`,
-  //   },
-  // } as Metadata,
   announcement: {
     applicationName: 'StudyMate',
     title: '공지 사항',
