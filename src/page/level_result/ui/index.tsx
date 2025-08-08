33a78e3d0d385cd1759ed24202b6848cdf7f5efe
dinn54 <dinn543210@gmail.com>
2025-08-08 13:11:42 +0900

    style: :lipstick: update testresult page ui

--- src/page/level_result/ui/index.tsx
+++ src/page/level_result/ui/index.tsx
@@ -1,55 +1,53 @@
 'use client';
-
 import { GetLevelTestResultRes } from '@/page/level_test/api';
-import TestResultContent from './testResultContent';
-import CountUp from '@/feature/CountUp/CountUp';
+
+import ButtonPixel from '@/shared/button/buttonPixel';
+import Link from 'next/link';
+import { RouteTo } from '@/shared/routes/model/getRoutePath';
+import LevelTestCategoryProblems from './categoryProblems';
+import ResultSummary from './resultSummary';
+import useTestResultContent from '../model/testResultHook';
+import { Spinner } from '@/feature/spinner/ui/spinnerUI';
 
 export interface ResultData extends GetLevelTestResultRes {
   userAnswers: number[];
 }
 
 const TestResultPage = () => {
-  const correct = 16;
-  const total = 20;
-  const rate: number = Number(((correct / total) * 100).toFixed(2));
+  const { questionInfos, resultData } = useTestResultContent();
+
+  const correct = resultData?.correctQuestions.length ?? 0;
+  const total = resultData?.requestedQuestionCount ?? 1;
+
+  const rate =
+    resultData?.percentileScore ?? Number(((correct / total) * 100).toFixed(2));
+
+  const problemListsArray = Object.entries(questionInfos).map(
+    ([key, value]) => ({
+      id: key,
+      ...value,
+    })
+  );
+  const correctIds = new Set(resultData?.correctQuestions ?? []);
+  const wrongIds = new Set(resultData?.wrongQuestions ?? []);
+
+  if (problemListsArray.length === 0) {
+    return <Spinner size="lg" />;
+  }
 
   return (
-    <div className="flex h-full w-full flex-col px-4 py-5">
+    <div className="flex h-full w-full flex-col overflow-y-auto px-4 py-5 scrollbar-hide">
       {/* <TestResultContent /> */}
       <span className="text-title-page">Result</span>
-      <span className="mt-6 text-[96px] leading-none">
-        <CountUp
-          from={0}
-          to={rate < 0 || rate > 100 ? 0 : rate}
-          direction="up"
-          delay={0.2}
-          duration={1}
-        />
-        %
-      </span>
-      <div className="relative mt-2 flex h-1 w-full rounded-[2px]">
-        {rate < 0 || rate > 100 ? null : (
-          <>
-            <div
-              className={`absolute left-0 top-0 h-1 w-full rounded-[2px] bg-white transition-all duration-1000 ease-in-out`}
-              style={{
-                left: String(rate >= 99 ? 99 : rate + 1) + '%',
-                width: String(rate >= 99 ? 100 - rate : 100 - rate - 1) + '%',
-              }}
-            />
-            <div
-              className="absolute left-0 top-0 h-1 w-0 rounded-[2px] bg-point-orange transition-all duration-1000 ease-in-out"
-              style={{ width: String(rate) + '%' }}
-            />
-          </>
-        )}
-      </div>
-      <span className="mt-3 text-[20px] font-bold leading-[1.15] text-point-orange">
-        Correct: {correct}/{total}
-      </span>
-      <span className="text-[20px] font-bold leading-[1.15]">
-        Incorrect: {total - correct}/{total}
-      </span>
+      <ResultSummary correct={correct} total={total} rate={rate} />
+      <LevelTestCategoryProblems
+        correctIds={correctIds}
+        wrongIds={wrongIds}
+        problemsIdWithTitle={problemListsArray}
+      />
+      <ButtonPixel status="default">
+        <Link href={RouteTo.Home}> Go to Home </Link>
+      </ButtonPixel>
     </div>
   );
 };
--- src/page/level_result/ui/testResultContent.tsx
+++ src/page/level_result/ui/testResultContent.tsx
@@ -27,9 +27,9 @@ const TestResultContent = () => {
     isLoading,
   } = useTestResultContent();
 
-  // if (!isLoading) {
-  //   return <Spinner size="xl" />;
-  // }
+  if (!isLoading) {
+    return <Spinner size="xl" />;
+  }
 
   return (
     <div className="flex h-full w-full max-w-[1200px] flex-col items-center gap-6 overflow-y-scroll rounded-2xl p-3 shadow-[0_8px_30px_rgb(0,0,0,0.06)] scrollbar-hide md:justify-center">
@@ -52,13 +52,6 @@ const TestResultContent = () => {
         <div className="flex h-full w-full flex-col gap-5">
           <button className="hidden items-center justify-between rounded-md bg-[#FEBA73] p-3 text-white md:flex">
             자세히 보기
-            {/* <div className="h-auto w-[40px]">
-              <SvgIcon
-                inheritViewBox
-                component={Arrow}
-                sx={{ width: '100%', height: '100%' }}
-              />
-            </div> */}
           </button>
           <div className="grid grid-cols-2 flex-col gap-[0.1rem] overflow-y-scroll rounded-lg bg-none p-0.5 scrollbar-hide md:flex md:h-[40vh] md:bg-white md:shadow-md">
             {filteredProblems.map((problem, index) => {
