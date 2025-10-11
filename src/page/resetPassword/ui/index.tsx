258bf2c15a2cee590268491ef5ff7a551d75c553
dinn54 <dinn543210@gmail.com>
2025-10-12 00:51:44 +0900

    feat: 비밀번호 리셋 페이지 추가

--- src/page/resetPassword/ui/index.tsx
+++ src/page/resetPassword/ui/index.tsx
@@ -1,29 +1,76 @@
+'use client';
 import HomeLogo from '@/feature/images/ui/homelogo';
 import ButtonPixel from '@/shared/button/buttonPixel';
 import InputForm from '@/shared/input/inputForm';
+import { sendResetPasswordEmailApi } from '../api';
+import { FormEvent, useState } from 'react';
+import { SvgIcon } from '@mui/material';
+import EmailIcon from '@public/assets/icons/resetPassword/email.svg';
+import Link from 'next/link';
+import { RouteTo } from '@/shared/routes/model/getRoutePath';
 
 const ResetPasswordPage = () => {
+  const [email, setEmail] = useState<string>();
+
+  const sendPasswordResetLink = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formEl = (e.target as HTMLFormElement).elements.namedItem(
+      'email'
+    ) as HTMLInputElement;
+    const email = formEl.value;
+    try {
+      const res = await sendResetPasswordEmailApi(email);
+      if (res.ok) {
+        setEmail(email);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <div className="flex h-full w-full flex-col items-center justify-center px-4 text-black dark:text-white">
-      <div className="flex w-full flex-col justify-center">
-        <HomeLogo />
-        <span className="mt-6 whitespace-pre-line text-center font-pretandard">
-          {`스터디메이트에 가입했던 이메일을 입력해주세요
+      {!email ? (
+        <div className="flex w-full flex-col justify-center">
+          <HomeLogo />
+          <span className="mt-6 whitespace-pre-line text-center font-pretandard">
+            {`스터디메이트에 가입했던 이메일을 입력해주세요
 비밀번호 재설정 메일을 보내드립니다.`}
-        </span>
-        <InputForm
-          type="email"
-          status="empty"
-          name="email"
-          placeholder="Enter your Email"
-          className="mt-14 font-pretandard text-label"
-        />
-        <div className="mt-4">
-          <ButtonPixel type="button" status="default">
-            Send Email
-          </ButtonPixel>
+          </span>
+          <form onSubmit={async (e) => await sendPasswordResetLink(e)}>
+            <InputForm
+              type="email"
+              status="empty"
+              name="email"
+              placeholder="Enter your Email"
+              className="mt-14 font-pretandard text-label"
+            />
+            <div className="mt-4">
+              <ButtonPixel type="submit" status="default">
+                Send Reset Link
+              </ButtonPixel>
+            </div>
+          </form>
         </div>
-      </div>
+      ) : (
+        <div className="flex w-full flex-col items-center justify-center text-center">
+          <SvgIcon
+            component={EmailIcon}
+            inheritViewBox
+            sx={{ width: 'auto', height: '40px' }}
+          />
+          <div className="mt-6 w-full font-pretandard">
+            <span className="line-[28px] text-[20px] text-point-orange">
+              {email}
+            </span>
+            <p className="mt-2 text-body-primary">{`비밀번호 재설정 메일이 발송되었습니다.`}</p>
+            <div className="mt-20 w-full">
+              <Link href={RouteTo.Home} className="w-full">
+                <ButtonPixel status="default">Okay</ButtonPixel>
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
