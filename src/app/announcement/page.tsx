636e66f61d33267c05b3457b5f9dbe46ff6f0718
dinn54 <dinn543210@gmail.com>
2025-10-27 10:06:38 +0900

    fix: 공지사항 페이지의 서버 컴포넌트트 비동기 자식 컴포넌트 분리

--- src/app/announcement/page.tsx
+++ src/app/announcement/page.tsx
@@ -1,30 +1,17 @@
-import AnnouncementPage from '@/page/announcement/ui';
-import PageAnimationWrapper from '@/shared/style/ui/pageAnimationWrapper';
 import { pageMetadata } from '@/pageMetadata';
-import { getAllNoticeListRes } from '@/feature/notice/api';
+
 import { Suspense } from 'react';
+import PageAnimationWrapper from '@/shared/style/ui/pageAnimationWrapper';
 import { PageLoader } from '@/feature/spinner/ui/pageLoader';
-import { _serverFetch } from '../api/v1/_serverFetch';
-import * as Sentry from '@sentry/nextjs';
+import AnnouncementServerFetchWrapper from '@/page/announcement/ui/serverFetchWrapper';
 
 export const metadata = pageMetadata.announcement;
-export const revalidate = 0;
-const Announcement = async () => {
-  const res = await _serverFetch<getAllNoticeListRes>({
-    method: 'GET',
-    path: '/api/v1/notice?page=0&limit=10',
-  });
 
-  console.log('final response', res.payload);
+const Announcement = () => {
   return (
     <PageAnimationWrapper>
       <Suspense fallback={<PageLoader />}>
-        <AnnouncementPage
-          noticeList={
-            'content' in res.payload &&
-            (res.payload as getAllNoticeListRes).content
-          }
-        />
+        <AnnouncementServerFetchWrapper />
       </Suspense>
     </PageAnimationWrapper>
   );
