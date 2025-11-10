fe8a7db817ca0a8ea831bb9aae9a00cbc72901c8
dinn54 <dinn543210@gmail.com>
2025-11-10 10:55:02 +0900

    fix: 어드민 페이지 기본 모드 라이트 모드로 변경

--- src/page/adminLogin/index.tsx
+++ src/page/adminLogin/index.tsx
@@ -2,10 +2,15 @@
 import Logo from '@public/assets/backgroundImages/main/logo.svg';
 import AdminLoginForm from './ui/adminLoginForm';
 import AuthHoc from '@/shared/auth/model/authHoc';
+import { useEffect } from 'react';
 
 const AdminPage = () => {
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', false);
+  });
+
   return (
-    <div className="relative flex w-full min-w-[320px] flex-col items-center justify-center p-5 md:w-[80rem] md:flex-row md:p-0">
+    <div className="relative flex w-full !min-w-[600px] flex-col items-center justify-center p-5 md:w-[80rem] md:flex-row md:p-0">
       <div className="flex w-full max-w-[30rem] items-end justify-center md:w-[50%] md:max-w-none md:justify-end md:p-[3rem]">
         <div className="relative flex aspect-1 w-[80%] rounded-full border-4 border-[#fafbe7]">
           <div className="flex h-full w-full items-end justify-center">
